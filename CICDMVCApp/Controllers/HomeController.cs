86b1ecdd5786c19536ce7e60f2ad3cdb3caf34c6
lokeshOabh <89448118+lokeshOabh@users.noreply.github.com>
2023-09-25 21:10:43 +0530

    AB#47 Update HomeController.cs

--- CICDMVCApp/Controllers/HomeController.cs
+++ CICDMVCApp/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace CICDMVCApp.Controllers
 {
     public class HomeController : Controller
     {
+    // commit - 1
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
